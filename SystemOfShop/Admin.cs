58855445c2c4bf6578e87fd8b5011bec57620ac8
student <student@B303-10>
2023-01-26 09:04:12 +0700

    Добавьте файлы проекта.

---
+++ SystemOfShop/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemOfShop
+{
+    internal class Admin : User
+    {
+        public override bool Enter(string login, string password)
+        {
+            return base.Enter(login, password);
+        }
+    }
+}
---
+++ SystemOfShop/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemOfShop
+{
+    internal class User
+    {
+        private string name;
+        private string login;
+        private string password;
+        public void Users(string name, string login, string password)
+        {
+            name = "Сергей";
+            login = "Sergey";
+            password = "12345678";
+        }
+        public virtual bool Enter(string login, string password)
+        {
+            login = Console.ReadLine();
+            password = Console.ReadLine();
+            bool result;
+            if(login != null & password != null)
+            {
+                result = true;
+            }
+            else { result = false; }
+            return result;
+        }
+    }
+}
